88c14d76d1858e2433588db66b3cbfd2efaa2b0d
Vitalii Dolmatov <Dolmatov-vs@yandex.ru>
2020-07-31 09:42:24 +0300

    init project

---
+++ src/test/java/ru/netology/data/UserData.java
@@ -0,0 +1,58 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class UserData {
+    private UserData(){
+    }
+
+    @Value
+    public static class UserInfo {
+        private String city;
+        private String date;
+        private String fullName;
+        private String phone;
+    }
+
+    public static UserInfo getUserInfo() {
+        Faker faker = new Faker(new Locale("ru"));
+        return new UserInfo(
+                getCity(),
+                dateMeeting(),
+                faker.name().lastName()+" "+faker.name().firstName(),
+                faker.phoneNumber().phoneNumber());
+    }
+
+    public static String dateMeeting() {
+        Faker faker = new Faker(new Locale("ru"));
+
+        final String dateFormatWithDots = "dd.MM.yyyy";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormatWithDots);
+
+        int daysToAdd = faker.random().nextInt(5, 365);
+        LocalDate newDateMeeting = LocalDate.now().plusDays(daysToAdd);
+        String dateMeeting = newDateMeeting.format(formatter);
+        return dateMeeting;
+    }
+
+    public static String getCity(){
+        String[] city = new String[]{"Майкоп", "Уфа, Улан-Удэ", "Махачкала", "Магас", "Нальчик", "Элиста",
+                "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск", "Якутск", "Владикавказ",
+                "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита",
+                "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь", "Ставрополь", "Хабаровск",
+                "Благовещенск", "Астрахань", "Белгород", "Брянск", "Владимир", "Волгоград", "Вологда",
+                "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово", "Киров", "Кострома",
+                "Курган", "Курск", "Санкт-Петербург", "Липецк", "Магадан", "Москва", "Красногорск",
+                "Нижний Новгород", "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза",
+                "Псков", "Ростов-на-Дону", "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург",
+                "Смоленск", "Тамбов", "Тверь","Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск",
+                "Ярославль", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард", "Биробиджан"};
+        return city[new Random().nextInt(city.length-1)];
+    }
+}
---
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -0,0 +1,139 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import org.junit.jupiter.api.*;
+
+import org.openqa.selenium.Keys;
+import ru.netology.data.UserData;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.chord;
+import static ru.netology.data.UserData.getUserInfo;
+
+public class CardOrderTest {
+
+    String selectAll = chord(Keys.CONTROL, "a");
+    Keys del = Keys.DELETE;
+
+    SelenideElement cityField = $("[data-test-id=city] input");
+    SelenideElement dateField = $("[data-test-id=date] input");
+    SelenideElement nameField = $("[data-test-id=name] input");
+    SelenideElement phoneField = $("[data-test-id=phone] input");
+    SelenideElement checkbox = $("[data-test-id=agreement]");
+    SelenideElement bookButton = $$(".form button").find(text("Запланировать"));
+    SelenideElement rescheduling = $("[data-test-id='replan-notification']");
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSuccessfulMeetingReservationTest() {
+        UserData.UserInfo user = getUserInfo();
+        cityField.setValue(user.getCity());
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getDate());
+        nameField.setValue(user.getFullName());
+        phoneField.setValue(user.getPhone());
+        checkbox.click();
+        bookButton.click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на"));
+        $("[data-test-id=success-notification]").shouldHave(text(user.getDate()));
+    }
+
+    @Test
+    void shouldMeetingRescheduledSuccessfully() {
+        UserData.UserInfo user = getUserInfo();
+        cityField.setValue(user.getCity());
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getDate());
+        nameField.setValue(user.getFullName());
+        phoneField.setValue(user.getPhone());
+        checkbox.click();
+        bookButton.click();
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на"));
+        $("[data-test-id=success-notification]").shouldHave(text(user.getDate()));
+        dateField.sendKeys(selectAll, del);
+        val date = UserData.dateMeeting();
+        dateField.setValue(date);
+        bookButton.click();
+        rescheduling.$("button").click();
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на"));
+        $("[data-test-id=success-notification]").shouldHave(text(date));
+    }
+
+    @Test
+    void shouldCityFieldIsRequired() {
+        UserData.UserInfo user = getUserInfo();
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getDate());
+        nameField.setValue(user.getFullName());
+        phoneField.setValue(user.getPhone());
+        checkbox.click();
+        bookButton.click();
+        $(".input_invalid[data-test-id=city]").shouldHave(text("Поле обязательно для заполнения"));
+        $(".input_invalid[data-test-id=city]").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(withText("Успешно!")).waitUntil(hidden, 15000);
+    }
+
+    @Test
+    void shouldDateFieldIsRequired(){
+        UserData.UserInfo user = getUserInfo();
+        cityField.setValue(user.getCity());
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getFullName());
+        phoneField.setValue(user.getPhone());
+        checkbox.click();
+        bookButton.click();
+        $(".calendar-input__custom-control").shouldHave(text("Неверно введена дата"));
+        $(".calendar-input__custom-control").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(withText("Успешно!")).waitUntil(hidden, 15000);
+    }
+
+    @Test
+    void shouldNameFieldIsRequired(){
+        UserData.UserInfo user = getUserInfo();
+        cityField.setValue(user.getCity());
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getDate());
+        phoneField.setValue(user.getPhone());
+        checkbox.click();
+        bookButton.click();
+        $("[data-test-id=name]").shouldHave(text("Поле обязательно для заполнения"));
+        $("[data-test-id=name]").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(withText("Успешно!")).waitUntil(hidden, 15000);
+    }
+
+    @Test
+    void shouldTelFieldIsRequired(){
+        UserData.UserInfo user = getUserInfo();
+        cityField.setValue(user.getCity());
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getDate());
+        nameField.setValue(user.getFullName());
+        checkbox.click();
+        bookButton.click();
+        $("[data-test-id=phone]").shouldHave(text("Поле обязательно для заполнения"));
+        $("[data-test-id=phone]").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(withText("Успешно!")).waitUntil(hidden, 15000);
+    }
+
+    @Test
+    void shouldCheckboxFieldIsRequired(){
+        UserData.UserInfo user = getUserInfo();
+        cityField.setValue(user.getCity());
+        dateField.sendKeys(selectAll, del);
+        dateField.setValue(user.getDate());
+        nameField.setValue(user.getFullName());
+        phoneField.setValue(user.getPhone());
+        bookButton.click();
+        checkbox.shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        $(withText("Успешно!")).waitUntil(hidden, 15000);
+    }
+}
